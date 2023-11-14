1725edf9b98dcc413f6c07a6174fe5c9d116bf70
Taejun Woo <tjunne@naver.com>
2023-11-15 07:16:27 +0900

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+const OFF = 0;
+const WARNING = 1;
+const ERROR = 2;
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'prettier/prettier': WARNING,
+    '@typescript-eslint/interface-name-prefix': OFF,
+    '@typescript-eslint/explicit-function-return-type': OFF,
+    '@typescript-eslint/explicit-module-boundary-types': OFF,
+    '@typescript-eslint/no-explicit-any': OFF,
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: false,
+  endOfLine: 'auto',
+  singleQuote: true,
+  trailingComma: 'es5'
+}
